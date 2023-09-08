4b22ec33407c438e939bf3bdf92ae466b085515c
Mattia <info@mattiaorfano.com>
2023-09-08 16:15:10 +0200

    First commit

---
+++ spec/activerecord/hashremap_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activerecord::Hashremap do
+  it "has a version number" do
+    expect(Activerecord::Hashremap::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
